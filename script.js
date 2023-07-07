27c8230b07ddd0c58627d1bdf7e2a146b87a3376
evan-pieper <evanjpieper@gmail.com>
2023-07-07 00:28:48 -0700

    added timer and game over screen when you lose

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-let button = document.createElement("button");
-let score = document.createElement("h2");
-let timer = document.createElement("h2");
-let promptParagraph = document.createElement("p");
-let choiceButton = document.createElement("button");
+let highScoreContainer = document.getElementById("highScoreContainer");
+if (localStorage.getItem("highScore") === null) {
+  highScoreContainer.textContent = "High Score: NA";
+  localStorage.setItem("highScore", "High Score: NA")
+}
+else{
+  highScoreContainer.textContent = localStorage.getItem("highScore");
+}
 
 
+let questionNumber = document.getElementById("questionNumber");
+let timerDisplay = document.getElementById("timerDisplay");
+let questionText = document.getElementById("questionText");
+
+let startButton = document.getElementById("startButton");
+startButton.addEventListener("click", function () {
+  startButton.style.display = "none";
+  questionNumber.style.display = "block";
+  timerDisplay.style.display = "block";
+  questionText.style.display = "block";
+  startTimer();
+});
+
+function startTimer() {
+  let timeLeft = 2;
+  let timer = setInterval(function () {
+    if (timeLeft > 0) {
+      timerDisplay.textContent = "Time Remaining: " + timeLeft + "s";
+      timeLeft--;
+    }
+    else {
+      clearInterval(timer);
+      timerDisplay.textContent = "Time Remaining: 0s";
+      questionNumber.textContent = "Game Over!";
+    }
+  }, 1000);
+}
+
+let choicesContainer = document.getElementById("choicesContainer");
+let currentScore = document.getElementById("currentScore");
 
 
 // The following function renders items in a todo list as <li> elements
