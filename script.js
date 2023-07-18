2c6605ed86fc2f5c6d247b850f2ae8114a498b29
evan-pieper <evanjpieper@gmail.com>
2023-07-17 19:17:57 -0700

    finished js and html, now just need css styling, documentation, and comment code

--- script.js
+++ script.js
@@ -3,20 +3,32 @@ let currentScore = document.getElementById("currentScore");
 let questionNumber = document.getElementById("questionNumber");
 let timerDisplay = document.getElementById("timerDisplay");
 let questionText = document.getElementById("questionText");
+let gameRunning = true;
 
 let startButton = document.getElementById("startButton");
-
-let highScoreContainer = document.getElementById("highScoreContainer");
-if (localStorage.getItem("highScore") === null) {
-  highScoreContainer.textContent = "High Score: BB: -1";
-  localStorage.setItem("highScore", "High Score: BB: -1")
+const BB = {scorer: "BB", score: -1};
+let highScoreArray = [BB]; //array of highscorer objects with there scores attached
+let highScoreList = document.getElementById("highScoreList");
+if (localStorage.getItem("highScoreArray") === null) {
+  localStorage.setItem("highScoreArray", JSON.stringify(highScoreArray));
+  renderHighScores();
 }
-else{
-  highScoreContainer.textContent = localStorage.getItem("highScore");
+else {
+  renderHighScores();
 }
-
 let timeLeft = 90;
 
+function renderHighScores(){
+  highScoreList.innerHTML = ""; //clears the highscore list
+  let highScoreArray = JSON.parse(localStorage.getItem("highScoreArray"));
+  highScoreArray.sort(function(a, b){return b.score - a.score});
+  highScoreArray = highScoreArray.slice(0, 5);
+  for (let i = 0; i < highScoreArray.length; i++) {
+    let highScore = document.createElement("li");
+    highScore.textContent = highScoreArray[i].scorer + ": " + highScoreArray[i].score + "s";
+    highScoreList.appendChild(highScore);
+  }
+}
 
 // ____________________________________________________________________________
 // let choicesContainer = document.getElementById("choicesContainer");
@@ -27,13 +39,16 @@ let timeLeft = 90;
 
 //let startButton = document.getElementById("startButton");
 startButton.addEventListener("click", function () {
+  gameRunning = true;
+  questionText.textContent = questionArray[0];
   startButton.style.display = "none";
   questionNumber.style.display = "block";
+  timeLeft = 90;
   timerDisplay.style.display = "block";
   questionText.style.display = "block";
   choicesContainer.style.display = "grid";
   for(var choice in choicesContainer){
-    if(choice === "null" || choice === "undefined" || choice === ""){
+    if(choice === "null" || choice === "undefined"){
       return;
     }
     if(choice.textContent !== "undefined" && choice !== "null" && choice !== ""){
@@ -50,14 +65,18 @@ startButton.addEventListener("click", function () {
 function startTimer() {
   timeLeft = 89;
   let timer = setInterval(function () {
-    if (timeLeft > 0) {
+    if (gameRunning === true) {
       timerDisplay.textContent = "Time Remaining: " + timeLeft + "s";
       timeLeft--;
+      currentScore.textContent = "Current Score: " + timeLeft;
     }
     else {
       clearInterval(timer);
-      timerDisplay.textContent = "Time Remaining: 0s";
-      questionNumber.textContent = "Game Over!";
+    }
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      gameOver();
     }
   }, 1000);
 }
@@ -103,6 +122,8 @@ function buttonClick() {
   }
   if(questionNumber.textContent === "Question 4"){
     questionText.textContent = questionArray[3];
+    choice3.style.display = "none";
+    choice4.style.display = "none";
     choice1.textContent = q4Choices[0];
     choice2.textContent = q4Choices[1];
     choice3.textContent = q4Choices[2];
@@ -110,6 +131,8 @@ function buttonClick() {
   }
   if(questionNumber.textContent === "Question 5"){
     questionText.textContent = questionArray[4];
+    choice3.style.display = "none";
+    choice4.style.display = "none";
     choice1.textContent = q5Choices[0];
     choice2.textContent = q5Choices[1];
     choice3.textContent = q5Choices[2];
@@ -120,37 +143,67 @@ function buttonClick() {
 let choicesButtons = document.querySelectorAll(".choices");
 choicesButtons.forEach((item, index, array) =>  
 {
-  console.log(item);
+  //console.log(item.textContent + " event listener added");
   item.addEventListener("click", function () 
   {
-    console.log(item.textContent);
-    console.log(this);
+    console.log(item.textContent + " clicked");
+    //console.log(this);
     let questionIndex = questionArray.indexOf(questionText.textContent);
-    if (this.textContent !== correctAnswerArray[questionIndex]) {
-      if(timeLeft - 5 <= 0){
+    if (this.textContent !== correctAnswerArray[questionIndex]) { //if the button clicked is not the correct answer for the current question
+      if(timeLeft - 5 <= 0){ //if the time left is less than or equal to 5 seconds set it to zero and game over
         timeLeft = 0;
-        gameOver();
       }
-      else{
+      else{ //otherwise subtract 5 seconds from the time left
         timeLeft = timeLeft-5;
       }
-      questionText.textContent = questionArray[questionIndex + 1];
-      questionNumber.textContent = "Question " + (questionIndex + 1);
     }
-    else {
-      currentScore.textContent = "Current Score: " + 0;
-      questionText.textContent = questionArray[questionIndex + 1];
+    else { //if the button clicked is the correct answer for the current question
+      if(questionIndex === 4){ //if the current question is the last question
+        gameOver();
+      }
+      else { //otherwise move on to the next question
+        questionNumber.textContent = "Question " + (questionIndex + 2);
+      }
     }
+    console.log(questionNumber.textContent);
     buttonClick();
   });
 });
 
 gameOver = function(){
+  gameRunning = false;
+  startButton.style.display = "block";
+  //console.log(timeLeft);
+  //console.log(localStorage.getItem("highScore"));
+  currentScore.textContent = "Current Score: " + timeLeft;
   questionNumber.textContent = "Game Over!";
-  timerDisplay.textContent = "Time Remaining: 0s";
-  questionText.textContent = "Your score is: " + currentScore.textContent;
-  if(currentScore.textContent > localStorage.getItem("highScore")){
-    localStorage.setItem("highScore", currentScore.textContent);
-    highScoreContainer.textContent = localStorage.getItem("highScore");
+  timerDisplay.textContent = "Time Remaining:" + timeLeft + "s";
+  questionText.textContent = "Your score is: " + timeLeft + "s";
+  highScoreArray = JSON.parse(localStorage.getItem("highScoreArray"));
+
+  if(highScoreArray.length < 5){
+    console.log("New high score!");
+    let highScoreNameIn = prompt("Enter your name");
+    let highScoreObject = {scorer: highScoreNameIn, score: timeLeft};
+    highScoreArray.push(highScoreObject);
+    localStorage.setItem("highScoreArray", JSON.stringify(highScoreArray));
+    renderHighScores();
+  }
+  else{
+    if(timeLeft >= highScoreArray[4].score){
+      console.log("New high score!");
+      let highScoreNameIn = prompt("Enter your name");
+      let highScoreObject = {scorer: highScoreNameIn, score: timeLeft};
+      highScoreArray.push(highScoreObject);
+      localStorage.setItem("highScoreArray", JSON.stringify(highScoreArray));
+      renderHighScores();
+
+    }
+    else{
+      console.log("No new high score");
+      localStorage.setItem("highScoreArray", JSON.stringify(highScoreArray));
+      renderHighScores();
+    }
+    buttonClick();
   }
 }
